3ce21b77252436832fb0ab931ec3bc65df01a97b
Stephen Wentling <swentling87@gmail.com>
2016-11-12 13:41:40 -0800

    iterative correction

--- 02-algorithms/01-introduction/fibonacci_iterative.rb
+++ 02-algorithms/01-introduction/fibonacci_iterative.rb
@@ -1,5 +1,16 @@
 def fib(n)
-  n < 2 ? (return n) : (fib( n - 1 ) + fib( n - 2 ))
+  if n == 0
+    return 0
+  else
+    fib_0 = 0
+    fib_1 = 1
+    (1..(n-1)).each do
+      temp = fib_0
+      fib_0 = fib_1
+      fib_1 = temp + fib_1
+    end
+    return fib_1
+  end
 end
 
 
